d6a4c79143b8cfbb58665a631bc64f6eaf20aada
fellyph <fellyph.cintra@gmail.com>
2017-12-19 00:18:40 +0000

    adding post-thumbnail support

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+add_theme_support( 'post-thumbnails');
+add_theme_support( 'custom-header' );
+add_theme_support( 'custom-logo' );
+
 class wp_ng_theme {
 
 	function enqueue_scripts() {
@@ -9,7 +13,7 @@ class wp_ng_theme {
 		wp_enqueue_script( 'angular-core', '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js', array( 'jquery' ), '1.0', false );
 		wp_enqueue_script( 'angular-resource', '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-resource.js', array('angular-core'), '1.0', false );
 		wp_enqueue_script( 'ui-router', '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js', array( 'angular-core' ), '1.0', false );
-		wp_enqueue_script( 'ngScripts', get_template_directory_uri() . '/assets/js/angular-theme.js', array( 'ui-router' ), '1.0', false );
+		wp_enqueue_script( 'ngScripts', get_template_directory_uri() . '/assets/javascript/angular-theme.js', array( 'ui-router' ), '1.0', false );
 		wp_localize_script( 'ngScripts', 'appInfo',
 			array(
 				'api_url'			 => rest_get_url_prefix() . '/wp/v2/',
@@ -29,11 +33,13 @@ function create_post_type() {
         'singular_name' => __( 'Trabalho' )
       ),
       'public' => true,
-      'has_archive' => true,
+			'has_archive' => true,
+			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
     )
   );
 }
 
 $ngTheme = new wp_ng_theme();
+
 add_action( 'wp_enqueue_scripts', array( $ngTheme, 'enqueue_scripts' ) );
 add_action( 'init', 'create_post_type' );
