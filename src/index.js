8a78330f884615c756b76f656199aaa097d9f10c
fellyph <fellyph.cintra@gmail.com>
2017-12-26 13:28:05 +0000

    adding new modules from yarn ui-router, bootstrap and angular

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-const abenilza = ['oi']
+import 'bootstrap'
 
-console.log(abenilza)
+import angular from 'angular'
+import uirouter from 'angular-ui-router'
+
+import routing from './app.config'
+
+let wpApp = new angular.module('wpAngularTheme', [uirouter])
+              .config(routing)
+
+/*
+wpApp.factory('Trabalhos', function ($resource) {
+  return $resource(appInfo.api_url + 'trabalhos/:ID', {
+    ID: '@id'
+  })
+})
+
+wpApp.controller('ListCtrl', ['$scope', 'Trabalhos', function ($scope, Trabalhos) {
+  $scope.page_title = 'Blog Listing'
+  Trabalhos.query(function (res) {
+    console.log(res)
+    $scope.trabalhos = res
+  })
+}])
+
+wpApp.controller('DetailCtrl', ['$scope', '$stateParams', 'Trabalhos', function ($scope, $stateParams, Posts) {
+  console.log('Detail')
+  $scope.page_title = 'Detail Post'
+  Posts.get({ID: $stateParams.id}, function (res) {
+    $scope.post = res
+  })
+}])
+
+wpApp.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+  $stateProvider
+    .state('list', {
+      url: '/',
+      controller: 'ListCtrl',
+      templateUrl: appInfo.template_directory + 'templates/trabalhos/list.html'
+    })
+    .state('detail', {
+      url: '/trabalhos/:id',
+      controller: 'DetailCtrl',
+      templateUrl: appInfo.template_directory + 'templates/trabalhos/detail.html'
+    })
+})
+
+wpApp.filter( 'to_trusted', ['$sce', function ($sce) {
+  return function (text) {
+    return $sce.trustAsHtml(text)
+  }
+}])
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -19,5 +20,16 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        // In case you imported plugins individually, you must also require them here:
+        Util: "exports-loader?Util!bootstrap/js/dist/util",
+        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+      })
+  ]
 }
